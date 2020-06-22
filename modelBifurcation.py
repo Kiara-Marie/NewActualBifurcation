9e6bcb8849f11d15a005356905c50191de6b9640
Kiara <kiarag@alumni.ubc.ca>
2020-06-21 17:06:48 -0700

    write to file

--- modelBifurcation.py
+++ modelBifurcation.py
@@ -7,24 +7,29 @@ from numpy.random import default_rng
 
 
 def runSim():
-    small_border = -5
-    big_border = 5
+    small_border = -10
+    big_border = 10
     fig = plt.figure()
     ax = p3.Axes3D(fig)
     line, = ax.plot(xs=points[:, 0], ys=points[:, 1],
                     zs=points[:, 2], ls='', marker='.')
     # Creating the Animation object
-    line_ani = animation.FuncAnimation(fig, update_points, frames=1000,
-                                       fargs=(line,),
-                                       interval=20, blit=False)
-
+    line_anim = animation.FuncAnimation(fig, update_points, frames=180,
+                                       fargs=(line,), repeat=False,
+                                       interval=10, blit=False)
     ax.set_xlim3d([small_border, big_border])
     ax.set_ylim3d([small_border, big_border])
     ax.set_zlim3d([small_border, big_border])
+
+    # # Set up formatting for the movie files
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=15, metadata=dict(artist='Kiara'), bitrate=1800)
+
+    line_anim.save('drifting_lobes.mp4', writer=writer)
     plt.show()
 
 
-def createEllipsoid(num_points=100, r=3):
+def createEllipsoid(num_points=1000, r=3):
     A = np.array([-1, 0, 0])
     B = np.array([1, 0, 0])
     rng = default_rng()
@@ -45,6 +50,9 @@ def getDists(point, mat):
 
 
 def update_points(num, line):
+    max = 30
+    if (num == max):
+        return None
     global points
     global speeds
     def acceleration_fun(vec):
@@ -54,9 +62,8 @@ def update_points(num, line):
     accelerations = np.apply_along_axis(acceleration_fun, 1, points)
     accelerations[accelerations > 0] = 0
     speeds = speeds + accelerations
-    speeds[speeds < 0] = 0
+    speeds[speeds < 0] = 0.05
     speeds_to_use = np.transpose(np.array([speeds, speeds, speeds]))
-    change_in_pos = np.multiply(speeds_to_use, np.apply_along_axis(lambda v: v / LA.norm(v), 1, points))
     points = points + (np.multiply(speeds_to_use, np.apply_along_axis(lambda v: v / LA.norm(v), 1, points)))
     line.set_data(np.transpose(points[:, 0:2]))
     line.set_3d_properties(np.transpose(points[:, 2]))
