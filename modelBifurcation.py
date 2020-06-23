1448bdbf8e98664e4796896598b3e996902e381f
Kiara <kiarag@alumni.ubc.ca>
2020-06-23 10:28:19 -0700

    good but slow

--- modelBifurcation.py
+++ modelBifurcation.py
@@ -14,7 +14,7 @@ def runSim():
     line, = ax.plot(xs=points[:, 0], ys=points[:, 1],
                     zs=points[:, 2], ls='', marker='.')
     # Creating the Animation object
-    line_anim = animation.FuncAnimation(fig, update_points, frames=180,
+    line_anim = animation.FuncAnimation(fig, update_fun, frames=10,
                                         fargs=(line,), repeat=False,
                                         interval=10, blit=False)
     ax.set_xlim3d([small_border, big_border])
@@ -29,7 +29,7 @@ def runSim():
     plt.show()
 
 
-def createEllipsoid(num_points=1000, r=3):
+def createEllipsoid(num_points=100, r=3):
     A = np.array([-1, 0, 0])
     B = np.array([1, 0, 0])
     rng = default_rng()
@@ -40,21 +40,30 @@ def createEllipsoid(num_points=1000, r=3):
     filtered_points = unfiltered_points[(total_dists <= r), :]
     return filtered_points
 
+def initialize_which_are_ions():
+    initial_ion_proportion = 0.3
+    num_ions = int(initial_ion_proportion * len(points))
+    which_are_ions = [False for i in range(len(points))]
+    which_are_ions[0:num_ions] = [True for i in range(num_ions)]
+    return which_are_ions
+
 
 def update_ion_densities():
     sphere_to_consider = 0.2
     global points
+    global which_are_ions
+    global local_ion_densities
     for p_index, point in enumerate(points):
         dists_to_point = get_dists(point, points)
         # how many points are within the max sphere of this point, and are ions? 
-        local_ion_density[p_index] = np.sum((dists_to_point <= sphere_to_consider) & which_are_ions)
+        which_are_close = dists_to_point <= sphere_to_consider
+        local_ion_densities[p_index] = np.sum(which_are_close & which_are_ions)
 
 
 def get_dists(point, mat):
 
     def minus_point_func(vec):
         return vec - point
-
     mat_minus_point = np.apply_along_axis(minus_point_func, 1, mat)
     dist_to_point = LA.norm(mat_minus_point, axis=1)
     return dist_to_point
@@ -63,7 +72,9 @@ def get_dists(point, mat):
 def update_points(num, line):
     global points
     global speeds
-    accelerations = np.apply_along_axis(acceleration_fun, 1, points)
+    global local_ion_densities
+    c_acceleration = -0.01
+    accelerations = local_ion_densities * temperature_e * c_acceleration
     accelerations[accelerations > 0] = 0
     speeds = speeds + accelerations
     speeds[speeds < 0] = 0.05
@@ -74,10 +85,15 @@ def update_points(num, line):
     return line,
 
 
+def update_fun(num, line):
+    update_points(num, line)
+    update_ion_densities()
+
+
 v_0 = 0.1
 points = createEllipsoid()
 speeds = np.ones(len(points)) * v_0
 temperature_e = 25
-local_ion_density = np.zeros(len(points))
+local_ion_densities = np.zeros(len(points))
 which_are_ions = initialize_which_are_ions()
 runSim()
